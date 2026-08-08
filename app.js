14afbd20580f388c29786e592c1f28d8cd89a24d
muromaru <79000953+muromaru@users.noreply.github.com>
2026-08-08 17:55:24 +0900

    app.js を更新

--- app.js
+++ app.js
@@ -1,11 +1,46 @@
 // 四国の中心付近
 const shikoku = [33.75, 133.5];
+
 // 地図を作成
 const map = L.map("map").setView(shikoku, 8);
-// OpenStreetMapの地図を使用
+
+// OpenStreetMap
 L.tileLayer(
     "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
     {
         attribution: '&copy; OpenStreetMap contributors'
     }
 ).addTo(map);
+
+
+// --------------------------------
+// 寺データを読み込む
+// --------------------------------
+
+fetch("data/temples.json")
+    .then(response => response.json())
+    .then(temples => {
+
+        // 88ヶ所を1件ずつ処理
+        temples.forEach(temple => {
+
+            // ピンを作る
+            const marker = L.marker([
+                temple.latitude,
+                temple.longitude
+            ]).addTo(map);
+
+            // ピンをタップしたときの情報
+            marker.bindPopup(`
+                <strong>第${temple.number}番 ${temple.name}</strong>
+                <br>
+                ${temple.prefecture}
+                <br>
+                ${temple.address}
+            `);
+        });
+
+    })
+    .catch(error => {
+        console.error("寺データの読み込みに失敗しました:", error);
+    });
